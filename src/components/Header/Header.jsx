c19ea276a3fc2e9db9d3e74bc7216fc80ca23d88
atenziono <atenziono@gmail.com>
2023-04-04 17:16:44 +0430

    hamburger menu and header: WIP

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import SvgBackground from "../../Public/SvgBackgroud";
 import { ReactComponent as Location } from "../../../public/location.svg";
 import { ReactComponent as Search } from "../../../public/Search.svg";
 import { ReactComponent as Cart } from "../../../public/Cart.svg";
+import { ReactComponent as Hamburger } from "../../../public/Hamburger.svg";
 
 import Style from './Header.module.scss'
 
 function Header() {
+    const [menu, setMenu] = useState('');
+
     return <div className={Style.container}>
         <div className={Style.content}>
             <div className={Style.headerText}>
-                <h2>Store</h2>
-                <div className={Style.navigationElement}><Location className={Style.navIcon} />Tehran</div>
-                <div className={Style.navigation}>
-                    <a>Products</a>
-                    <a>Blog</a>
-                    <a>Features</a>
-                    <a>Contact</a>
+                <div className={Style.rowResponsive}>
+                    <a onClick={() => {
+                        console.log('CLICK');
+                        setMenu(Style.navigationVisible);
+                    }}>
+                        <Hamburger className={Style.hamburgerIcon} />
+                    </a>
+                    <h2>Store</h2>
+                    <div className={Style.navigationElement}>
+                        <Location className={Style.navIcon} />
+                        <p>
+                            Tehran
+                        </p>
+                    </div>
                 </div>
-                <a className={Style.iconBtn}>
-                    <Search />
-                </a>
-                <a className={Style.iconBtn}>
-                    <Cart className={Style.iconBtn} />
-                </a>
-                <div>
-                    <a className={Style.btnMd}>Sign up</a>
+                <div className={Style.hamburgerMenu}>
+                    <div className={`${Style.navigation} ${menu}`}>
+                        <a>Products</a>
+                        <a>Blog</a>
+                        <a>Features</a>
+                        <a>Contact</a>
+                    </div>
+                </div>
+                <div className={Style.row}>
+
+                    <a className={Style.iconBtn}>
+                        <Search />
+                    </a>
+                    <a className={Style.iconBtn}>
+                        <Cart className={Style.iconBtn} />
+                    </a>
+                    <div>
+                        <a className={Style.btnMd}>Sign up</a>
+                    </div>
                 </div>
             </div>
         </div>
