6508e85b6bf49a20d6e3f33b1b2d3bdc9c744d01
atenziono <atenziono@gmail.com>
2023-06-06 15:23:20 +0430

    FIX:ListCustom component issue with home/index page routing has been fixed

--- src/class/LinkCustom.jsx
+++ src/class/LinkCustom.jsx
@@ -7,13 +7,21 @@ class LinkCustom extends React.Component {
     render() {
         const location = this.props.location;
         const to = this.props.to;
-        console.log(location, to);
         return (
+
             location.indexOf(to) >= 0 ?
-                <p>
-                    {this.props.children}
-                </p>
-                : <a href={this.props.to}                >
+                location == to ?
+                    <p>
+                        {this.props.children}
+                    </p>
+                    : <a
+                        href={this.props.to}
+                    >
+                        {this.props.children}
+                    </a>
+                : <a
+                    href={this.props.to}
+                >
                     {this.props.children}
                 </a>
         );
